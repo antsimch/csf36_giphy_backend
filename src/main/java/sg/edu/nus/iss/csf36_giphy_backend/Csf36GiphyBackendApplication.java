a4a6638e60373935722f83974fb0b772deab5c64
antsimch <anthonysimch@gmail.com>
2023-08-16 12:21:51 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/csf36_giphy_backend/Csf36GiphyBackendApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.csf36_giphy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csf36GiphyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csf36GiphyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/csf36_giphy_backend/Csf36GiphyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.csf36_giphy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csf36GiphyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
